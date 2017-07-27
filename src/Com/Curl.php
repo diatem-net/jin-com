24fe46ca76ca04fb62ac92280bda8561f8731631
= <lgerard@diatem.net>
2017-07-27 09:00:50 +0200

    Support des opération PUT/PATCH et DELETE

--- src/Com/Curl.php
+++ src/Com/Curl.php
@@ -28,15 +28,16 @@ class Curl
   const CURL_REQUEST_TYPE_POST = 'POST';
   const CURL_REQUEST_TYPE_PUT = 'PUT';
   const CURL_REQUEST_TYPE_DELETE = 'DELETE';
+  const CURL_REQUEST_TYPE_PATCH = 'PATCH';
 
   /**
    * Appelle une Url
 	 *
    * @param  string        $url                Url à appeler
    * @param  array|string  $args               (optional) Arguments à transmettre (NULL par défaut)
-   * @param  string        $requestType        (optional) Type de requête. (POST, GET, DELETE ou PUT) (POST par défaut)
+   * @param  string        $requestType        (optional) Type de requête. (POST, GET, DELETE, PATCH ou PUT) (POST par défaut)
    * @param  boolean       $throwError         (optional) Génère les erreurs directement (True par défaut)
-   * @param  string        $contentType        (optional) Header 'Content-type'. Par défaut : application/json
+   * @param  string        $contentType        (optional) Header 'Content-type'. Par défaut : multipart/form-data
    * @param  array         $headers            (optional) Headers additionnels. (Sous la forme d'un tableau clé/valeur)
    * @param  string        $outputTraceFile    (optional) Si renseigné : effectue une trace des flux réseaux dans le fichier ainsi déterminé. (Chemin absolu du fichier)
    * @param  boolean       $followLocation     (optional) Autorise CURL a suivre les redirections (False par défaut)
@@ -45,6 +46,11 @@ class Curl
    */
   public static function call($url, $args = null, $requestType = self::CURL_REQUEST_TYPE_POST, $throwError = true, $httpAuthUser = null, $httpAuthPassword = null, $contentType = null, $headers = array(), $outputTraceFile = null, $followLocation = false)
   {
+
+    if(!$contentType){
+      $contentType = 'multipart/form-data';
+    }
+
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_COOKIESESSION, true);
@@ -80,7 +86,7 @@ class Curl
       curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
       curl_setopt($curl, CURLOPT_USERPWD, $httpAuthUser . ':' . $httpAuthPassword);
     }
-
+    
     // Delete
     if ($requestType == self::CURL_REQUEST_TYPE_DELETE) {
       curl_setopt($curl, CURLOPT_CUSTOMREQUEST, self::CURL_REQUEST_TYPE_DELETE);
@@ -101,21 +107,31 @@ class Curl
       }
       curl_setopt($curl, CURLOPT_URL, $url);
     }
-
-    // Post
-    if ($requestType == self::CURL_REQUEST_TYPE_POST) {
-      curl_setopt($curl, CURLOPT_POST, TRUE);
-      curl_setopt($curl, CURLOPT_POSTFIELDS, $args);
-    }
-
-    // Put
-    if ($requestType == self::CURL_REQUEST_TYPE_PUT) {
-      curl_setopt($curl, CURLOPT_CUSTOMREQUEST, self::CURL_REQUEST_TYPE_PUT);
+    
+    // Patch
+    if ($requestType == self::CURL_REQUEST_TYPE_PATCH) {
+      curl_setopt($curl, CURLOPT_CUSTOMREQUEST, self::CURL_REQUEST_TYPE_PATCH);
       if (!empty($args)) {
         curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($args));
       }
     }
+    
+    // Post
+    if ($requestType == self::CURL_REQUEST_TYPE_POST) {
+      curl_setopt($curl, CURLOPT_POST, true);
+      curl_setopt($curl, CURLOPT_POSTFIELDS, $args);
 
+    }
+    
+    // Put
+    if ($requestType == self::CURL_REQUEST_TYPE_PUT) {
+      curl_setopt($curl, CURLOPT_CUSTOMREQUEST, self::CURL_REQUEST_TYPE_PUT);
+      if (!empty($args)) {
+         curl_setopt($curl, CURLOPT_POST, true);
+         curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($args));
+      }
+    }
+    
     $return = curl_exec($curl);
 
     self::$lastCurlInfo = curl_getinfo($curl);
